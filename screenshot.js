79179e9dea123b6bb7ce7fa6b55f92ec7d5a8c00
Mastinoo <dylan@fraiquin.eu>
2025-01-31 19:06:59 +0100

    just
    stupid

--- screenshot.js
+++ screenshot.js
@@ -1,4 +1,4 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const chromium = require('chromium'); // Import Chromium package
 const express = require('express');
 const app = express();
